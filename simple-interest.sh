4cae08b70cd18247058d537a262c94b8fdfd5b07
Chida <43975562+chidag108@users.noreply.github.com>
2022-06-17 12:57:40 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Chida Ghimiray
    # Additional Authors:
-   # <your GitHub username>
+   # <chidag108>
 
    # Input:
    # p, principal amount
